d185062377b5ff4f3213d74d081b516723af8258
Uminchu618 <uminchu@uminchu.jp>
2019-12-05 01:14:20 +0900

    create classs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={ ()=>{console.log("I am Clicked")}} />
-    </React.Fragment>
-  );
-}
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={ ()=>{console.log("I am Clicked")}} />
+//     </React.Fragment>
+//   );
+// }
 
+const App = () =>{
+  return <div>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+  </div>
+  }
+
+  const Cat = () =>{
+    return <div>Meow!</div>
+    }
+  
 export default App;
