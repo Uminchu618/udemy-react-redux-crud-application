ba357aa311ca91dfb232ba0240cb1a570cbc3d61
Uminchu618 <uminchu@uminchu.jp>
2019-12-11 15:52:41 +0900

    implement event new submit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS';
+export const CREATE_EVENT = 'CREATE_EVENT';
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -8,5 +9,12 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch =>{
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch( {type: READ_EVENTS, response}) //Reducerに対して送られる
+}
+
+export const postEvent = values => async dispatch =>{
+    console.log(values)
+    const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`,values)
+    console.log(response)
+    dispatch( {type: CREATE_EVENT, response}) //Reducerに対して送られる
 }
 
