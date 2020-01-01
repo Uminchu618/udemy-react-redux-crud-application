c40a94a9bb3c1872feb165769a4f696bb45bacc9
Uminchu618 <uminchu@uminchu.jp>
2020-01-01 16:04:55 +0900

    material-ui

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -5,44 +5,65 @@ import {Link} from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
+
+
 class EventsIndex   extends Component{
+
   componentDidMount()
   {
       this.props.readEvents();
   }
 
-renderEvents(){
+ renderEvents(){
   return _.map(this.props.events,event=>(
-    <tr key={event.id}>
-      <td>{event.id}</td>
-      <td>
+    <TableRow key={event.id}>
+      <TableCell>{event.id}</TableCell>
+      <TableCell>
         <Link to={`/events/${event.id}`}>{event.title}</Link>
-      </td>
-      <td>{event.body}</td>
-    </tr>
+      </TableCell>
+      <TableCell>{event.body}</TableCell>
+    </TableRow>
   ))
-}
+  }
 
   render(){
     return (
       <React.Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderEvents()}
-          </tbody>
-        </table>
-        <Link to="/events/new">New Event</Link>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Body</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.renderEvents()}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Button component={Link} to="/events/new" >New</Button>
       </React.Fragment>
     )
   }   
 }
+
+const classes = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 
 const mapStateToProps = state => ({events: state.events})
 
